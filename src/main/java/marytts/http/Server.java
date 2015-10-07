b632cfaad3aa8de9e0e48f5d047d2ab906fe70ce
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2015-10-07 19:44:29 +0200

    mary 6 start

--- src/main/java/marytts/http/Server.java
+++ src/main/java/marytts/http/Server.java
@@ -22,11 +22,16 @@ package marytts.http;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import marytts.exceptions.MaryConfigurationException;
+import marytts.util.MaryRuntimeUtils;
+
 @SpringBootApplication
 public class Server {
 
-    public static void main(String[] args)
+    public static void main(String[] args)  throws Exception
     {
+    
+                
         SpringApplication.run(Server.class, args);
     }
 }
