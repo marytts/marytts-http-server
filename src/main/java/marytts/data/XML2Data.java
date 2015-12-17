9d27b6d246e400c684718a0c6078a528c64d491f
Atta-Ur-Rehman Shah <supercomsian1989@gmail.com>
2015-12-17 13:24:55 +0100

    - added multiple user support

--- src/main/java/marytts/data/XML2Data.java
+++ src/main/java/marytts/data/XML2Data.java
@@ -4,13 +4,10 @@ package marytts.data;
 import java.util.ArrayList;
 
 /* XML */
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 
 /**
